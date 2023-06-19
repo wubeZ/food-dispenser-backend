f0a13397d09738068abc3dcf479622215c0b21a1
mac <wubezeleke@gmail.com>
2023-06-19 21:58:25 +0300

    bug fixes

--- src/resources/Schedule/controller.js
+++ src/resources/Schedule/controller.js
@@ -41,8 +41,8 @@ const create = async (date, dateString, chickens, amount, device) => {
             logger.info(`feeding ${entry.chickens} chickens ${entry.amount} grams of food failed`); 
           }
           else{
-          // client.publish(`feedingRequest/${entry.device}`, feedingrepsonse);
-          client.publish(`feedingRequest/${entry.device}`,'', {retain: true});
+          client.publish(`feedingRequest/${entry.device}`, feedingrepsonse);
+          // client.publish(`feedingRequest/${entry.device}`,'', {retain: true}); // for deleting retained message
           logger.info(`feeding ${entry.chickens} chickens ${entry.amount} grams of food`);
           schedules.date.setDate(schedules.date.getDate() + 1)
           if ((feeding.endDate === null ) || ((feeding.endDate - schedules.date) >= 0) ){
